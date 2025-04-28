3fc92950f39419ae16e2dedbd5f6ef9419c8c607
simoneru <simonerufo3@gmail.com>
2025-04-28 14:50:34 +0200

    now we hava our first rotating cube

--- code/rotatingCube/cube.c
+++ code/rotatingCube/cube.c
@@ -11,9 +11,12 @@
 #include <windows.h>
 #include <windowsx.h>
 #include <gl/gl.h>
+#include <math.h>
 
 static BOOL Running = TRUE;
 static HGLRC OpenGLRC;
+static float Angle = 0.0f; // cube rotation angle
+static DWORD lastTime = 0; // last frame timestamp
 
 HGLRC InitOpenGL(HWND hWnd)
 {
@@ -44,6 +47,8 @@ HGLRC InitOpenGL(HWND hWnd)
     // Create an OpenGL rendering context using the device context
     HGLRC OpenGLRC = wglCreateContext(hWndDC);
 
+    glShadeModel(GL_SMOOTH);
+
     // Make the OpenGL context current (active) for the device context
     // OpenGL is now ready to use
 
@@ -69,16 +74,132 @@ void DestroyOpenGL(HGLRC OpenGLRC)
     }
 }
 
+void SetPerspective(float fovY, float aspect, float zNear, float zFar)
+{
+    float fH = tanf(fovY / 360.0f * 3.14159f) * zNear;
+    float fW = fH * aspect;
+    
+    glFrustum(-fW, fW, -fH, fH, zNear, zFar);
+}
 
 void DisplayBufferInWindow(HDC DeviceContext, int WindowWidth, int WindowHeight)
 {
+	// framerate handling
+	DWORD currentTime = GetTickCount(); 
+	float deltaTime = (currentTime - lastTime) / 1000.0f;
+	lastTime = currentTime;
+
 	//Platform independet OpenGL functions:
 	glViewport(0,0, WindowWidth, WindowHeight); // set the viewport
-	glClearColor(0.5f, 1.0f, 0.3f, 0.0f); // specify clear values for the color buffers
-	glClear(GL_COLOR_BUFFER_BIT); //  clear buffers to preset values
+	
+	// camera setup
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	SetPerspective(45.0f, (float)WindowWidth / (float)WindowHeight, 0.1f, 100.0f);
+
+	// transformation setup
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+	glTranslatef(0.0f, 0.0f, -5.0f); // move cube back
+	glRotatef(Angle, 1.0f, 1.0f, 0.0f); // rotate cube
+
+
+	glClearColor(0.129837f, 0.283764f, 0.54235f, 0.0f); // specify clear values for the color buffers
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); //  clear buffers to preset values + Depth buffers (Z-Buffers is a property that  the device uses to store depth infos)
+	
+	glEnable(GL_DEPTH_TEST); // Enable depth test for 3D
+
+	// draw a cube
+	glBegin(GL_QUADS);
+	
+	// Front face (z+)
+	glColor3f(1, 0, 0); // Red
+	glVertex3f(-1, -1, 1);
+	
+	glColor3f(1, 1, 0); // Yellow
+	glVertex3f(1, -1, 1);
+	
+	glColor3f(0, 1, 0); // Green
+	glVertex3f(1, 1, 1);
+	
+	glColor3f(0, 1, 1); // Cyan
+	glVertex3f(-1, 1, 1);
+	
+	// Back face (z-)
+	glColor3f(1, 0, 1); // Magenta
+	glVertex3f(-1, -1, -1);
+	
+	glColor3f(0, 0, 1); // Blue
+	glVertex3f(-1, 1, -1);
+	
+	glColor3f(0, 1, 1); // Cyan
+	glVertex3f(1, 1, -1);
+	
+	glColor3f(1, 1, 0); // Yellow
+	glVertex3f(1, -1, -1);
+	
+	// Left face (x-)
+	glColor3f(1, 0, 1); // Magenta
+	glVertex3f(-1, -1, -1);
+	
+	glColor3f(1, 0, 0); // Red
+	glVertex3f(-1, -1, 1);
+	
+	glColor3f(0, 1, 0); // Green
+	glVertex3f(-1, 1, 1);
+	
+	glColor3f(0, 0, 1); // Blue
+	glVertex3f(-1, 1, -1);
+	
+	// Right face (x+)
+	glColor3f(1, 1, 0); // Yellow
+	glVertex3f(1, -1, -1);
+	
+	glColor3f(0, 1, 1); // Cyan
+	glVertex3f(1, 1, -1);
+	
+	glColor3f(0, 1, 0); // Green
+	glVertex3f(1, 1, 1);
+	
+	glColor3f(1, 0, 0); // Red
+	glVertex3f(1, -1, 1);
+	
+	// Top face (y+)
+	glColor3f(0, 1, 1); // Cyan
+	glVertex3f(-1, 1, -1);
+	
+	glColor3f(0, 1, 0); // Green
+	glVertex3f(-1, 1, 1);
+	
+	glColor3f(1, 0, 0); // Red
+	glVertex3f(1, 1, 1);
+	
+	glColor3f(0, 0, 1); // Blue
+	glVertex3f(1, 1, -1);
+	
+	// Bottom face (y-)
+	glColor3f(1, 0, 1); // Magenta
+	glVertex3f(-1, -1, -1);
+	
+	glColor3f(1, 1, 0); // Yellow
+	glVertex3f(1, -1, -1);
+	
+	glColor3f(1, 0, 0); // Red
+	glVertex3f(1, -1, 1);
+	
+	glColor3f(0, 1, 1); // Cyan
+	glVertex3f(-1, -1, 1);
+	
+	glEnd();
+	
+	
 	// The SwapBuffers function exchanges the front and back buffers if the current pixel format for the window
 	// referenced by the specified device context includes a back buffer.
 	SwapBuffers(DeviceContext); 
+
+	Angle += 30.0f * deltaTime; // 45 degrees per second
+	if(Angle >= 360.0f) Angle -= 360.0f;
+
 }
 
 void OnDestroy(HWND hWnd)
@@ -155,7 +276,9 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR szCmdLine,
 	
 	if(OpenGLRC)
 	{
-		InitOpenGL(hWnd);
+		
+        lastTime = GetTickCount();
+
 		Running = TRUE;
 		while(Running)
 		{	
@@ -171,6 +294,9 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR szCmdLine,
 				TranslateMessage(&msg);
 				DispatchMessage(&msg);
 			}
+
+			InvalidateRect(hWnd, NULL, FALSE);
+			Sleep(10);
 		}
 
 		DestroyOpenGL(OpenGLRC);
